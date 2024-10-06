1e8bc7bcedc835acf2d079f4443f5248c9f71092
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2024-10-06 17:06:46 -0300

    added h2-db config

---
+++ src/main/java/io/github/felixdev/acesso_api/AcessoApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.felixdev.acesso_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/felixdev/acesso_api/AcessoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felixdev.acesso_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
